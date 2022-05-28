499969c79ef5e24de9d496f0f08b0d657a7163a4
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-05-28 01:09:14 -0400

    Create flags_translator.py

---
+++ flags_translator.py
@@ -0,0 +1,17 @@
+def translate(flag: int):
+  flags = {
+    131072: 'VERIFIED_DEVELOPER',
+    65536: 'VERIFIED_BOT',
+    16384: 'BUG_HUNTER_LEVEL_2',
+    4096: 'SYSTEM',
+    1024: 'TEAM_USER',
+    512: 'PREMIUM_EARLY_SUPPORTER',
+    256: 'HYPESQUAD_ONLINE_HOUSE_3',
+    128: 'HYPESQUAD_ONLINE_HOUSE_2',
+    64: 'HYPESQUAD_ONLINE_HOUSE_1',
+    8: 'BUG_HUNTER_LEVEL_1',
+    4: 'HYPESQUAD',
+    2: 'PARTNER',
+    1: 'STAFF'
+  }
+  return flags[flag]
